60a85b84d713236e56aea2a4dd8390859cc132b8
Alex Manning <alexander.manning@stfc.ac.uk>
2023-04-24 14:40:16 +0100

    Build a bootstrap5 version of orgtheme_ceda_serv.
    Mirror the methods used in https://github.com/cedadev/orgtheme-jasmin/tree/bootstrap5

--- orgtheme_ceda_serv/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Organisation theme for CEDA Services web components.
-"""
-
-__author__ = "Matt Pritchard"
-__copyright__ = "Copyright 2018 UK Science and Technology Facilities Council"
-
-__version__ = "0.2"
